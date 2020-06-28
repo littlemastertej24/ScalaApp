9b6f22742b65e82d074b9079c3c598d1c754a383
Renuka Prasad P <rppasupulati@gmail.com>
2020-06-28 15:57:28 +0530

    Integrated Jenkins with GitHub

--- src/main/scala/com/rs/deepu/HelloWorld.scala
+++ src/main/scala/com/rs/deepu/HelloWorld.scala
@@ -5,5 +5,6 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello World Program")
     println("Build to Integrate Maven project with Jenkins")
+    println("Integrated Jenkins Project with GitHub")
   }
 }
